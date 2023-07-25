4586ccf7969d9cf4724d22184e945fac40bf9bbb
Jineesh Bhaskaran <93069439+jinu043@users.noreply.github.com>
2023-07-25 14:11:35 +0400

    Update webdevelopment_attempt_01.py

--- webdevelopment_attempt_01.py
+++ webdevelopment_attempt_01.py
@@ -92,13 +92,20 @@ with st.container():
                 unsafe_allow_html=True)
 
 with st.container():
-    st.subheader("Actual Price Details")
-    # selected_data = data[(data["vehicle_name"] == df.loc[0, "vehicle_name"]) & (data["model"] == df.loc[0, "model"]) &
-    #                 (data["dealer_name"] == df.loc[0, "dealer_name"])].reset_index(drop=True)
-    actual_price = data[(data["vehicle_name"]==df.loc[0,"vehicle_name"])&(data["model"]==df.loc[0,"model"])&
-                  (data["vehicle_type"]==df.loc[0,"vehicle_type"])&(data["export_status"]==df.loc[0,"export_status"])&
-                  (data["dealer_name"]==df.loc[0,"dealer_name"])&(data["model_year"]==df.loc[0,"model_year"])&(data["location"]==df.loc[0,"location"])&
-                  (data["fuel_type"]==df.loc[0,"fuel_type"])&(data["mileage"]==df.loc[0,"mileage"])&(data["spec"]==df.loc[0,"spec"])][["vehicle_name", "model", "price"]].reset_index(drop=True)
-    # record = pd.concat([selected_data,actual_price], axis=1)
-    st.dataframe(actual_price)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Actual Price Details")
+        # selected_data = data[(data["vehicle_name"] == df.loc[0, "vehicle_name"]) & (data["model"] == df.loc[0, "model"]) &
+        #                 (data["dealer_name"] == df.loc[0, "dealer_name"])].reset_index(drop=True)
+        actual_price = data[(data["vehicle_name"]==df.loc[0,"vehicle_name"])&(data["model"]==df.loc[0,"model"])&
+                      (data["vehicle_type"]==df.loc[0,"vehicle_type"])&(data["export_status"]==df.loc[0,"export_status"])&
+                      (data["dealer_name"]==df.loc[0,"dealer_name"])&(data["model_year"]==df.loc[0,"model_year"])&(data["location"]==df.loc[0,"location"])&
+                      (data["fuel_type"]==df.loc[0,"fuel_type"])&(data["mileage"]==df.loc[0,"mileage"])&(data["spec"]==df.loc[0,"spec"])][["vehicle_name", "model", "price"]].reset_index(drop=True)
+        # record = pd.concat([selected_data,actual_price], axis=1)
+        st.dataframe(actual_price)
+        with col2:
+        count = str(data[data["dealer_name"]==df.loc[0,"dealer_name"]].shape[0])
+        st.write("### Total Vehicles Count : ")
+        st.write(data[data["dealer_name"]==df.loc[0,"dealer_name"]]["dealer_name"].iloc[0])
+        st.write(count)
            
